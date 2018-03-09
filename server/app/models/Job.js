da7f28607dcaefa3d1484993edd9d0e09b0dd256
Alan Liu <alanl.liu@yahoo.com>
2018-03-09 13:48:47 +0100

    updated schemas with additional fields

--- server/app/models/Job.js
+++ server/app/models/Job.js
@@ -11,5 +11,39 @@ module.exports = new Schema({
 		type: String,
 		required: true
 	},
+	time: {
+		type: Date,
+		required: true
+	},
+	location: {
+		type: String,
+		required: true
+	},
+	number_required: {
+		type: Number,
+		required: true
+	},
+	contact_info: {
+		type: String,
+		required: true
+	},
+	skills_required: {
+		type: String,
+		required: false
+	},
+	payment_amount: {
+		type: Number,
+		required: false
+	},
+	confirmed: {
+		type: Boolean,
+		default: false,
+		required: false
+	},
+	paid: {
+		type: Boolean,
+		default: false,
+		required: false
+	},
 	author: [{type: Schema.ObjectId, ref: "User"}]
 });
