55342619909ff8ba41365e32f85bacb23cbb2e36
Adam Hirst <adam@ahirst.com>
2018-03-09 10:46:35 +0000

    Routes

---
+++ server/app/routes.js
@@ -0,0 +1,7 @@
+module.exports = (app, passport, db) => {
+
+  app.get('/', (req, res) => {
+    res.send('Hello world!');
+  })
+
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 const express  = require('express');
 const app      = express();
 const mongoose = require('mongoose');
-//const passport = reuqire('passport');
+const passport = reuqire('passport');
+const session  = require('express-session');
+const mstore   = require('connect-mongodb-session')(session);
 
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser  = require('cookie-parser');
+const bodyParser    = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+	store: store,
+	secret: 'hackvat',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Register db
 var db = require('./app/database.js')(mongoose);
 
+// Static directory
 app.use(express.static('public'));
 
+// Register routes
+require('./app/routes.js')(app, passport, db);
 
-
+// Set app to listen
 app.listen(3000, () => { console.log('Listening on 3000'); });
